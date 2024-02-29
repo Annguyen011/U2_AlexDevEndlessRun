50eb529e5c80a5fc433c3589dbb6002d0c78ff9a
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-01 01:55:37 +0700

    Finish gender

--- Assets/_Data/Scripts/Level/LevelGenderater.cs
+++ Assets/_Data/Scripts/Level/LevelGenderater.cs
@@ -8,16 +8,41 @@ namespace U2
     {
         [Header("# Level infos")]
         [SerializeField] private Transform[] levelPart;
-        [SerializeField] private Transform respanPos;
-
+        [SerializeField] private Vector3 netPartPos;
+        [SerializeField] private float distanceToSpan;
+        [SerializeField] private float distanceToDelete;
+        [SerializeField] private Transform player;
 
         private void Update()
         {
-            Transform part = levelPart[Random.Range(0, levelPart.Length)];
+            GenderatePlatform();
 
-            Transform nePart = Instantiate(part, respanPos.position, respanPos.rotation, transform);
+            DeletePlatform();
+        }
 
+        private void GenderatePlatform()
+        {
+            while (Vector2.Distance(player.position, netPartPos) < distanceToSpan)
+            {
 
+                Transform part = levelPart[Random.Range(0, levelPart.Length)];
+
+                Transform nePart = Instantiate(part, netPartPos - part.Find("StartPoint").position, transform.rotation, transform);
+
+                netPartPos = part.Find("EndPoint").position;
+            }
+        }
+
+        private void DeletePlatform()
+        {
+            if (transform.childCount == 0) return;
+
+            Transform partToDelete  = transform.GetChild(0);
+
+            if(Vector2.Distance(player.transform.position, partToDelete.position) > distanceToDelete)
+            {
+                Destroy(partToDelete.gameObject);
+            }
         }
     }
 }
