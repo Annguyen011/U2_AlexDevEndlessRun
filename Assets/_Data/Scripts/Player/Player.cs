434b6fa612c9657d569b54344913da439b9109be
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-01 08:28:23 +0700

    Add sliding

--- Assets/_Data/Scripts/Player/Player.cs
+++ Assets/_Data/Scripts/Player/Player.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 namespace U2
 {
     [RequireComponent(typeof(Rigidbody2D))]
-    [RequireComponent (typeof(BoxCollider2D))]
-    [RequireComponent (typeof(Animator))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Animator))]
     public class Player : MonoBehaviour
     {
         #region [Abilities]
@@ -17,12 +17,24 @@ namespace U2
 
         [Header("# Jump infos")]
         [SerializeField] private float jumpForce;
+        [SerializeField] private float doubleJumpForce;
+        private bool canDoubleJump;
 
+        [Header("# Slide infos")]
+        [SerializeField] private float slideSpeed;
+        [SerializeField] private float slideTime;
+        private float slideTimeCounter;
+        private bool isSliding;
 
         [Header("# Collision check")]
         [SerializeField] private LayerMask whatisground;
         [SerializeField] private float groundCheckDistance;
+        [Space]
+        [SerializeField] private Transform wallCheck;
+        [SerializeField] private Vector2 wallCheckSize;
         private bool isGrounded;
+        private bool wallDetected;
+
 
         [Header("# Animator")]
         [SerializeField] private string moveAnim;
@@ -46,7 +58,7 @@ namespace U2
 
         private void Start()
         {
-            
+
         }
 
         private void Update()
@@ -55,14 +67,25 @@ namespace U2
             Movement();
             CollisionCheck();
 
+            // Slide
+            slideTimeCounter -= Time.deltaTime;
+
+            if (isGrounded)
+            {
+                canDoubleJump = true;
+            }
+
+            CheckForSlide();
             CheckInput();
         }
 
-       
+
 
         private void OnDrawGizmos()
         {
-            Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+            Gizmos.DrawLine(transform.position, new Vector2(transform.position.x,
+                transform.position.y - groundCheckDistance));
+            Gizmos.DrawWireCube(wallCheck.position, wallCheckSize);
         }
 
         #endregion
@@ -73,8 +96,8 @@ namespace U2
             anim.SetFloat("yVelocity", rb.velocity.y);
 
             anim.SetBool("isGrounded", isGrounded);
-            //anim.SetBool("canDoubleJump", canDoubleJump);
-            //anim.SetBool("isSliding", isSliding);
+            anim.SetBool("canDoubleJump", canDoubleJump);
+            anim.SetBool("isSliding", isSliding);
             //anim.SetBool("canClimb", canClimb);
             //anim.SetBool("isKnocked", isKnocked);
 
@@ -84,24 +107,78 @@ namespace U2
 
         private void CollisionCheck()
         {
-            isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatisground);
+            isGrounded = Physics2D.Raycast(transform.position, Vector2.down,
+                groundCheckDistance, whatisground);
+
+            wallDetected = Physics2D.BoxCast(wallCheck.position,
+                wallCheckSize, 0, Vector2.zero, 0, whatisground);
         }
 
         private void CheckInput()
         {
-            if (Input.GetButtonDown("Jump") && isGrounded)
+            if (Input.GetButtonDown("Jump"))
             {
+                JumpButton();
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                SlideButton();
+            }
+        }
+
+        #region [Basic move]
+
+        private void JumpButton()
+        {
+            if (isGrounded)
+            {
+
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+            else if (canDoubleJump)
+            {
+                canDoubleJump = false;
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             }
         }
 
         private void Movement()
         {
-            if(!playerUnlock)
+            if (!playerUnlock && wallDetected)
             {
                 return;
             }
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+
+            if (isSliding)
+            {
+
+                rb.velocity = new Vector2(slideSpeed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            }
         }
+        #endregion
+
+        #region [Abilities]
+        // Slide
+
+        private void SlideButton()
+        {
+            slideTimeCounter = slideTime;
+            isSliding = true;
+        }
+
+        private void CheckForSlide()
+        {
+            if(slideTimeCounter <= 0)
+            {
+                isSliding = false;
+            }
+        }
+
+        #endregion
     }
 }
